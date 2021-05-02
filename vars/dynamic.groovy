d4ee2e1ee8859893065ad984fab380655ed32559
zguil <guilhermeferreiraraujo@hotmail.com>
2021-05-02 11:42:15 -0300

    Add files

--- vars/dynamic.groovy
+++ vars/dynamic.groovy
@@ -2,7 +2,7 @@ import br.com.core.PipeSteps
 
 def call(String repositor) {
     node {
-        git url: repositor
+        git url: repositor,
 
         stage ("Deploy Google Cloud") {
             sh gcloud app deploy
